9b6cbb2f1490e3a54587325cbfca408be4498c38
lafhielzakaria <lafhielzakaria@gmail.com>
2025-11-11 13:50:39 +0100

    display the data from json

--- formation-page.js
+++ formation-page.js
@@ -1,2 +1,42 @@
-let file = "./formation-page.json";
-fetch(file)
+let output = ""
+// 9ad line dyal derution 
+let cursedetails = document.getElementById("curse-title")
+fetch("./data/formation.json")
+
+   .then(res => res.json())
+   .then(formation => {
+
+      for (let item of formation) {
+
+         output += `
+            
+            <div style="box-shadow: 10px 10px rgba(134, 119, 119, 0.085); "
+          class="grid grid-cols-1 border border-gray-200 rounded-xl curse-card">
+          <img src="./public/coding.jpg" width="100%" height="30%" alt="html curse">
+
+          <div id="course-details" class="p-10 grid  gap-1  curse-details">
+
+               <p class="grid grid-rows-1" id="curse-title">${item.theme}</p>
+                <p id="formater-name">${item.trainer} </p>
+                
+                <p class = "grid grid-cols-2"  id="course-duration">${item.duration}h
+                 <span><img height="25px" width="25px" src="public/three-o-clock-clock.png"></span></p>
+                ${item.participants.length}/${item.capacity}</p>
+            <button class="grid place-content-center"> <a class=" grid  place-items-center  btn" href="#">Start The
+                course</a></button>
+          </div>
+   
+     `;
+
+
+
+         document.querySelector("#courses-details").innerHTML = output
+      }
+      // cursesdetails.array.forEach( form=> {
+      //    let cursedetail = form[form.name]
+      //    console.log(cursedetail)
+
+      // });
+
+
+   })
