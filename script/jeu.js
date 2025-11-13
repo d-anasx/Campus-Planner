97f5597d52a90b081c3b1ebcfef8e4a74dfda6dc
laaouafi ishak <laaouafi6@gmail.com>
2025-11-13 15:49:16 +0100

    j'ajoute le footer

--- script/jeu.js
+++ script/jeu.js
@@ -1,30 +1,40 @@
-// let qsts = [{
-//     nom: "html",
-//     hint: "Je suis le squelette d'une page web. J'utilise des balises comme <h1> et <p> pour définir la structure et le contenu."
-// },
-// {
-//     nom: "css",
-//     hint: "Je suis la peau et les vêtements d'une page web. Je contrôle les couleurs, les polices, la mise en page et l'apparence visuelle."
-// },
-// {
-//     nom: "javascript",
-//     hint: "Je suis le cerveau et les muscles d'une page web. J'ajoute l'interactivité, comme les animations, les réponses aux boutons et le contenu dynamique."
-// },
-// {
-//     nom: "frontend",
-//     hint: "Je suis la partie du site web que vous voyez et avec laquelle vous interagissez directement dans votre navigateur."
-// },
-// {
-//     nom: "backend",
-//     hint: "Je suis le côté serveur du site web.Je gère la base de données, la logique et les processus qui se déroulent en coulisses."
-// }
-// ]
 
-function affichage(words) {
+let scoreHolder
+let scoreCounter
+function localStorageCheck() {
+    const scoreHistoryObj = localStorage.getItem("user");
+    userScore = scoreHistoryObj ? JSON.parse(scoreHistoryObj) : { id: "", score: " " }
+}
+function affichageDuclavier(words, qstSelected, keys, keyBoard) {
     const indexAleatoire = Math.floor(Math.random() * words.length);
+    qstSelected.push(words[indexAleatoire])
 
+    hints.textContent = qstSelected[0].hint
+
+    keys.forEach(k => {
+        keyBoard.innerHTML += `<button id="btn" class="btn" >${k}</button>`
+    })
+}
+function affichageDuscore(tentative, tentativeCount) {
+    scoreCounter = Number(userScore.score)
+    scoreHolder = document.getElementById("score")
+    scoreHolder.innerHTML = "votre score: " + scoreCounter
+    tentative.innerHTML = "tentatives:  " + tentativeCount
+}
+function transformMot(qstSelected) {
+    let rep = qstSelected[0].word
+    console.log(qstSelected[0].word)
+    let rep2 = rep.split('')
+    let rep3 = rep.split('')
+    for (let i = 0; i < rep2.length; i++) {
+        rep3[i] = '_'
+    }
+
+    return {rep , rep2, rep3};
+}
+function affichage(words) {
     let qstSelected = []
-    let keys = ["a", "z", "e", "r", "t", "y", "u", "i", "o", "p", "q", "s", "d", "f", "g", "h", "j", "k", "l", "m", "w", "x", "c", "v", "b", "n"]
+    let keys = "azertyuiopqsdfghjklmwxcvbn".split('')
     let userScore = {
         id: "",
         score: 0
@@ -35,8 +45,7 @@ function affichage(words) {
     let hints = document.getElementById("hints")
     let word = document.getElementById("word")
     let reset = document.getElementById("resetbutton")
-    let scoreCounter
-    reset.onclick = function reset () {
+    reset.onclick = function reset() {
         userScore.score = 0
         localStorage.setItem("user", JSON.stringify(userScore))
         scoreCounter = userScore.score
@@ -46,39 +55,19 @@ function affichage(words) {
     next.onclick = function () {
         window.location.reload()
     }
-    const scoreHistoryObj = localStorage.getItem("user");
-    userScore = scoreHistoryObj ? JSON.parse(scoreHistoryObj) : { id: "", score: " " }
-
-    qstSelected.push(words[indexAleatoire])
-    // console.log(qstSelected[0])
-    hints.textContent = qstSelected[0].hint
-
-    keys.forEach(k => {
-        keyBoard.innerHTML += `<button id="btn" class="btn" >${k}</button>`
-    })
-    scoreCounter = Number(userScore.score)
-    let scoreHolder = document.getElementById("score")
-    scoreHolder.innerHTML = "votre score: " + scoreCounter
-    tentative.innerHTML = "tentatives:  " + tentativeCount
-    let rep = qstSelected[0].word
-    console.log(qstSelected[0].word)
-    // let rep1 = rep.split('')
-    let rep2 = rep.split('')
-    let rep3 = rep.split('')
-    for (let i = 0; i < rep2.length; i++) {
-        rep3[i] = '_'
-    }
-    let temp
+    localStorageCheck()
+    affichageDuclavier(words, qstSelected, keys, keyBoard)
+    affichageDuscore(tentative, tentativeCount)
+    const {rep, rep2, rep3} = transformMot(qstSelected)
     word.textContent = rep3.join("")
+    let temp
     let button = document.querySelectorAll("#btn")
     button.forEach(buttn => {
         buttn.addEventListener('click', function () {
 
             for (let i = 0; i < rep2.length; i++) {
                 if (buttn.textContent == rep2[i]) {
-                    temp = rep2[i]
-                    rep2[i] = rep3[i]
-                    rep3[i] = temp
+                    rep3[i] = buttn.textContent
                     word.textContent = rep3.join("")
                     buttn.setAttribute("class", "btn2")
                     // console.log(rep1)
@@ -88,11 +77,9 @@ function affichage(words) {
                         userScore.score = scoreCounter
                         button.forEach(buttn => {
                             buttn.disabled = true
-
                         })
                         localStorage.setItem("user", JSON.stringify(userScore))
                     }
-
                 }
                 else if (buttn.getAttribute('class') !== "btn2") {
                     buttn.setAttribute("class", "btn3")
@@ -107,10 +94,10 @@ function affichage(words) {
                 tentative.innerHTML = "game over"
                 button.forEach(buttn => {
                     buttn.disabled = true
-                    
+
 
                 })
-                
+
                 // document.getElementById("btn").disabled=true
 
             }
